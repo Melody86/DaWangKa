265a5a84ae9b762eb02ac42eaf87c9577beb7c2f
Lincoln <renfeide112@gmail.com>
2021-08-03 10:22:31 +0800

    update

--- src/router/router.config.js
+++ src/router/router.config.js
@@ -7,39 +7,9 @@ export const constantRouterMap = [
     redirect: { name: process.env.VUE_APP_TEMP }
   },
   {
-    path: '/chuangyuan_no_sel',
-    name: 'chuangyuan_no_sel',
-    component: () => import('@/views/chuangyuan_no_sel/index'),
-    meta: { title: '创源不选号', keepAlive: false }
-  },
-  {
-    path: '/liandong',
-    name: 'liandong',
-    component: () => import('@/views/liandong/index'),
-    meta: { title: '联动不选号', keepAlive: false }
-  },
-  {
-    path: '/chuangyuan',
-    name: 'chuangyuan',
-    component: () => import('@/views/chuangyuan/index'),
-    meta: { title: '创源', keepAlive: false }
-  },
-  {
-    path: '/dawang',
-    name: 'dawang',
-    component: () => import('@/views/dawang/index'),
-    meta: { title: '联通大王卡', keepAlive: false }
-  },
-  {
-    path: '/yidong',
-    name: 'yidong',
-    component: () => import('@/views/yidong/index'),
-    meta: { title: '电信星卡', keepAlive: false }
-  },
-  {
-    path: '/lingka',
-    name: 'lingka',
-    component: () => import('@/views/lingka/index'),
-    meta: { title: '电信靓卡', keepAlive: false }
+    path: '/chuangyuan_new',
+    name: 'chuangyuan_new',
+    component: () => import('@/views/chuangyuan_new/index'),
+    meta: { title: '创源20210803', keepAlive: false }
   }
 ]
