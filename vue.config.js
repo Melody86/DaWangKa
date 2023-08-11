76688ebcde1a84987774d42de77c80e68ef8b2fb
Lincoln <renfeide112@gmail.com>
2023-08-11 11:34:08 +0800

    对对对

--- vue.config.js
+++ vue.config.js
@@ -45,7 +45,7 @@ module.exports = {
   lintOnSave: !IS_PROD,
   productionSourceMap: false, // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
   devServer: {
-    port: 9020, // 端口
+    port: 8000, // 端口
     open: false, // 启动后打开浏览器
     disableHostCheck: true,
     overlay: {
